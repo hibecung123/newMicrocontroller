e8ac742747ba7faa63f7b4187c34c9d046088ec1
hibecung123 <son.nguyen1612@hcmut.edu.vn>
2022-10-18 15:46:04 +0700

    lab2_ex1

---
+++ Lab2/Exercise_1/Core/Inc/timer.h
@@ -0,0 +1,15 @@
+/*
+ * timer.h
+ *
+ *  Created on: Oct 2, 2022
+ *      Author: HELLO SON
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+extern int timer1_flag;
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_TIMER_H_ */
