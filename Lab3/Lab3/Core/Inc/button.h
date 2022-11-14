910a81764bba9a3be3ff4c397118451b6bf0a16c
hibecung123 <son.nguyen1612@hcmut.edu.vn>
2022-11-14 09:54:59 +0700

    lab3

---
+++ Lab3/Lab3/Core/Inc/button.h
@@ -0,0 +1,14 @@
+/*
+ * button.h
+ *
+ *  Created on: Nov 2, 2022
+ *      Author: HELLO SON
+ */
+
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+#include "main.h"
+#include "timer.h"
+void button_reading(void);
+int is_button_pressed(int index);
+#endif /* INC_BUTTON_H_ */
---
+++ Lab3/Lab3/Core/Inc/fsm_automatic.h
@@ -0,0 +1,15 @@
+/*
+ * fsm_automatic.h
+ *
+ *  Created on: Nov 2, 2022
+ *      Author: HELLO SON
+ */
+
+#ifndef INC_FSM_AUTOMATIC_H_
+#define INC_FSM_AUTOMATIC_H_
+
+#include "main.h"
+#include "global.h"
+void fsm_automatic();
+void initial();
+#endif /* INC_FSM_AUTOMATIC_H_ */
